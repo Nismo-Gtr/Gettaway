1d56832eaefc71fce3c36922fd03ee644faf16fb
James McGinn <jamangutan@gmail.com>
2018-05-03 12:33:38 -0500

    added a means to check the firebase

--- assets/application/app.js
+++ assets/application/app.js
@@ -1,8 +1,13 @@
 // Make the DOM Dynamic!
-$(document).ready(function() {
+$(document).ready(function () {
     //store linked firebase as the variable database
     var database = firebase.database();
-    
+
+    database.ref().on("value", function (snapshot) {
+        //display the .val() of a snapshot generated upon change of value at the rootlvl
+        console.log(snapshot.val());
+
+    })
 
 
 });
