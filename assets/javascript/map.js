666b53ca1daa3d82dd2a62be8e6f51f3432f32d8
James McGinn <jamangutan@gmail.com>
2018-05-07 11:14:49 -0500

    html-layout_rough-draft

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
-    console.log("herro");
+    console.log("workin!");
 
+ 
 
     $("button").on("click", function () {
         event.preventDefault();
+        
         $('#map').show();
+        $(function() {
+            var mapOptions = {
+              zoom: 8,
+              center: new google.maps.LatLng(41.881832, -87.623177)
+            };
+            var map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+          
+            // listen for the window resize event & trigger Google Maps to update too
+            $(window).resize(function() {
+              // (the 'map' here is the result of the created 'var map = ...' above)
+              google.maps.event.trigger(map, "resize");
+            });
+          });
+        
 
+        
     })
+    
 
+    
 
 });
 
