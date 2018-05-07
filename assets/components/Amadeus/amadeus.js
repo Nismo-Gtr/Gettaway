464dc7a8167c38b5c627867a2f25fcaf697b30cf
Nismo-Gtr <NCowan3@outlook.com>
2018-05-07 14:13:35 -0500

    Flight Inspiration API functionality

--- assets/components/Amadeus/amadeus.js
+++ assets/components/Amadeus/amadeus.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
   $("#submit").on("click", function () {
 
-    var origin = $("#origin").val().trim();
-    var oneWay = +$("#one-way").is(":checked");
-    var roundTrip = $("#round-trip").val();
-    
-    if (oneWay === 0) {
-      tripType = false;
-    }  
-  
- console.log(tripType);   
+    var origin = $("#origin").val();
+    var oneWay =  $("#one-way").prop("checked");
+    var maxPrice = $("#max_price").val();
+    var destination = $("#destination").val();
+
+    $("#round-trip").on("click", function() {
+
+    })
  
-    var queryURL = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=ZTIOSLgexgAGIo05oZ3xhfPDfQIVNXwh&origin=" + origin;
+    var queryURL = "https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?apikey=ZTIOSLgexgAGIo05oZ3xhfPDfQIVNXwh&origin=" + origin + "&destination=" + destination + "&direct=" + oneWay + "&max_price=" + maxPrice + "&number_of_results=20";
 
         $.ajax({
           url: queryURL,
