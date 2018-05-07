7818886bdefbcf0eba699f6b2f612c072d3d0c15
Dalamo <dalamo90@gmail.com>
2018-05-07 12:49:10 -0500

    added results table. *js is commented out in model js for manipulation. *html table added in model for manipulation.

--- assets/components/modal/modal.js
+++ assets/components/modal/modal.js
@@ -47,5 +47,64 @@ $(document).ready(function () {
             zipCode: userZipCode
         })
         });
+//this is results for modal
+// $(document).ready(function () {
+//     console.log("herro");
+  
+//     var database = firebase.database();
+//     var arr = [];
+  
+    
+//     database.ref().on("value", function(snapshot) {
+//         var data = snapshot.val() || {};// {} replaces if statement
+//         $("#flights").empty();
+  
+//         var keys = Object.keys(data);//returns array of strings
+  
+//         for (var i = 0; i < key.length; i++) {
+//             var flightArrival = data[keys[i]];
+//             var airline = "";
+//             var flightDetail = "";
+//             var cost = "";
+  
+//             //get these elements onto HTML table 
+//             var row = $("<tr>");//create a row for results to line up in
+//             //create columns/data that will go in the row
+//             var tLine = $("<td>");//this will hold airline name
+//             var tDetail = $("<td>"); //this will hold length of flight
+//             var tCost = $("<td>"); //this is flight cost
+//             //get data to stay in a single row
+//             row.append(tLine);
+//             row.append(tDetail);
+//             row.append(tCost);
+//             //display input in html
+//             tLine.text(flightArrival.airline);
+//             tDetail.text(flightArrival.flightDetail);
+//             tCost.text(flightArrival.cost);
+//             //get rows in tbody
+//             $("#flights").append(row);
+  
+//         }
+//     });
+//     //on submit we generate userInput data
+//     $("#search").on("click", function(event) {
+//         event.preventDefault();
+//         // $('#map').show();
+//         //these are elements to go in 
+//         var userInput = $("#searchBarInputId").val().trim();
+  
+//         database.ref().push({
+//             airline: userInput,
+//             flightDetail: userInput,
+//             cost: userInput
+//         });
+//         //this will clear text box when user clicks
+//         $("#searchBarInputId").val("");
+//     })
+  
+  
+//   });
+
+
     });
 
